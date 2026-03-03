c579553ed1ad1addf3ead58a26daf181b547e93c
this <339727042@GoTVDSB.ca>
2026-03-03 13:16:58 -0500

    pretty much done the bank yippee

--- looping assignment/Program.cs
+++ looping assignment/Program.cs
@@ -3,6 +3,7 @@
     internal class Program
     {
         public static string enter = "Press [ENTER] to continue";
+        public static int sleep;
         public static Random generator = new Random();
 
         static void Main(string[] args)
@@ -16,7 +17,7 @@
             for (int i = 0; i < text.Length; i++)
             {
                 Console.Write(text[i]);
-                Thread.Sleep(25);
+                Thread.Sleep(sleep);
             }
             Console.WriteLine();
 
@@ -89,16 +90,30 @@
             Console.Clear();
         }
 
+        public static double transactionFee = 0.75, tax = 1.13, cashBalance = 50, balance = 150, transCount = 0;
+        public static double depositAmt = 0, withdrawAmt = 0, billsAmt = 0;
+
         public static void ATM()
         {
             bool done = false; // false for while loop
-            double transactionFee = 0.75, tax = 1.13, cashBalance = 75, balance = 150; // given numerical values
             double deposit, payBill, withdraw; // inputs
             int transMenu;
-
+            balance = 10; cashBalance = 35;
             Console.WriteLine("-------------PLANET BLORB ATM-------------");
             while (!done)
             {
+                if (balance <= 0.74)
+                {
+                    Console.WriteLine("You have ran out of money.");
+                    Console.WriteLine("Your account will be closed.");
+                    Console.WriteLine();
+                    Console.WriteLine($"You are required to pay {transactionFee.ToString("C")} or more to open it again.");
+                    Console.WriteLine();
+                    Console.Write(enter);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                sleep = 25;
                 TypeText("Welcome to Bank of Blorb.");
                 TypeText("1. Deposit");
                 TypeText("2. Withdrawal");
@@ -107,15 +122,9 @@
                 TypeText("5. Quit");
                 Console.WriteLine();
                 Console.Write("Choose your transaction: ");
-                while ((!Int32.TryParse(Console.ReadLine(), out transMenu)))
+                while ((!Int32.TryParse(Console.ReadLine(), out transMenu)) || ((transMenu <= 0) || (transMenu > 5)))
                 {
-                    balance = balance - transactionFee;
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid Input.");
-                    Console.WriteLine("You have been charged $0.75.");
-                    Console.WriteLine($"Your balance is now {balance.ToString("C")}.");
-                    Console.WriteLine();
-                    Console.Write("Choose your transaction: ");
+                    AccountBalance();
                 }
                 Console.WriteLine();
                 Console.Write(enter);
@@ -125,88 +134,180 @@
                 switch (transMenu)
                 {
                     case 1: // deposit
-                        balance = balance - transactionFee;
-                        Console.WriteLine("-------------DEPOSIT-------------");
-                        Console.WriteLine($"You only have {cashBalance.ToString("C")} to deposit.");
-                        Console.Write("How much are you depositing into your bank? ");
-                        while ((!Double.TryParse(Console.ReadLine(), out deposit)) || ((deposit > cashBalance) || (deposit < 0)))
-                        {
-                            Console.WriteLine("Invalid Input.");
-                            Console.Write("How much are you depositing into your bank? ");
-                        }
-
-                        cashBalance = cashBalance - deposit;
-                        balance = balance + deposit;
-
-                        Console.WriteLine();
-                        Console.WriteLine("Click 'Account Balance Update' for your new balance.");
-                        Console.WriteLine();
-                        Console.Write(enter);
-                        Console.ReadLine();
-                        Console.Clear();
+                        Deposit();
                         break;
                     case 2: // withdrawal
-                        balance = balance - transactionFee;
-                        Console.WriteLine("-------------WITHDRAWAL-------------");
-                        Console.WriteLine("You must do an 'Account Balance Update' to check your balance.");
-                        Console.Write("How much are you withdrawing from your bank? ");
-                        while ((!Double.TryParse(Console.ReadLine(), out withdraw)) || ((withdraw > balance) || (withdraw < 0)))
-                        {
-                            Console.WriteLine("Invalid Input.");
-                            Console.Write("How much are you withdrawing from your bank? ");
-                        }
-
-                        balance = balance - withdraw;
-                        cashBalance = cashBalance + withdraw;
-
-                        Console.WriteLine();
-                        Console.Write(enter);
-                        Console.ReadLine();
-                        Console.Clear();
+                        Withdraw();
                         break;
                     case 3: // bill payments
-                        int outBill = generator.Next(0, 4);
-                        balance = balance - transactionFee;
-                        Console.WriteLine("-------------BILL PAYMENTS-------------");
-                        switch (outBill)
-                        {
-                            case 0:
-                                Console.WriteLine("You have no bills to pay.");
-                                Console.WriteLine("Please check back later.");
-                                break;
-                            case 1:
-                                Console.WriteLine("You have a mortgage to pay.");
-                                Console.WriteLine("It's $30 + tax.");
-                                break;
-                            case 2:
-                                Console.WriteLine("You have a hydro bill to pay.");
-                                Console.WriteLine("Your roommate already paid for most of it.");
-                                Console.WriteLine("It's $20 + tax.");
-                                break;
-                            case 3:
-                                Console.WriteLine("You have a data plan bill to pay.");
-                                Console.WriteLine("It's $40 + tax.");
-                                break;
-                        }
+                        Bills();
                         break;
                     case 4: // account balance
+                        AccountBalance();
                         break;
                     case 5: // quit
-
                         done = true;
                         break;
                     default: // else
-
-                        Console.WriteLine("Invalid Input.");
-                        Console.WriteLine("You have been charged $0.75.");
-                        Console.WriteLine($"Your balance is now {balance.ToString("C")}.");
-                        Console.WriteLine();
-                        Console.Write(enter);
-                        Console.ReadLine();
-                        Console.Clear();
+                        AccountBalance();
                         break;
                 }
             }
+        }
+
+        public static void Deposit()
+        {
+            double deposit;
+            transCount++;
+            balance = balance - transactionFee;
+            Console.WriteLine("-------------DEPOSIT-------------");
+            Console.WriteLine($"You only have {cashBalance.ToString("C")} to deposit.");
+            Console.Write("How much are you depositing into your bank? ");
+            while ((!Double.TryParse(Console.ReadLine(), out deposit)) || ((deposit > cashBalance) || (deposit < 0)))
+            {
+                Console.WriteLine("Invalid Input.");
+                Console.Write("How much are you depositing into your bank? ");
+            }
+
+            cashBalance = cashBalance - deposit;
+            balance = balance + deposit;
+            depositAmt += deposit;
+
+            Console.WriteLine();
+            Console.WriteLine("Click 'Account Balance Update' for your new balance.");
+            Console.WriteLine();
+            Console.Write(enter);
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public static void Withdraw()
+        {
+            double withdraw;
+            transCount++;
+            balance = balance - transactionFee;
+            Console.WriteLine("-------------WITHDRAWAL-------------");
+            Console.WriteLine("You must do an 'Account Balance Update' to check your balance.");
+            Console.Write("How much are you withdrawing from your bank? ");
+            while ((!Double.TryParse(Console.ReadLine(), out withdraw)) || ((withdraw > balance) || (withdraw < 0)))
+            {
+                Console.WriteLine("Invalid Input.");
+                Console.Write("How much are you withdrawing from your bank? ");
+            }
+
+            balance = balance - withdraw;
+            cashBalance = cashBalance + withdraw;
+            withdrawAmt += withdraw;
+
+            Console.WriteLine();
+            Console.Write(enter);
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public static void Bills()
+        {
+            double payBill = 0, total;
+            int outBill = generator.Next(0, 6);
+            string answer;
+            transCount++;
+            balance = balance - transactionFee;
+            Console.WriteLine("-------------BILL PAYMENTS-------------");
+            switch (outBill)
+            {
+                case 0:
+                    payBill = 0;
+                    Console.WriteLine("You have no bills to pay.");
+                    Console.WriteLine("Please check back later.");
+                    break;
+                case 1:
+                    payBill = 10;
+                    if (balance < 10)
+                    Console.WriteLine("You have a long distance call to pay.");
+                    Console.WriteLine("You called someone from 100 lightyears away, which costs money.");
+                    Console.WriteLine("It's $10 + tax.");
+                    break;
+                case 2:
+                    payBill = 20;
+                    Console.WriteLine("You have a hydro bill to pay.");
+                    Console.WriteLine("Your roommate already paid for most of it.");
+                    Console.WriteLine("It's $20 + tax.");
+                    break;
+                case 3:
+                    payBill = 30;
+                    Console.WriteLine("You have a mortgage to pay.");
+                    Console.WriteLine("It's $30 + tax.");
+                    break;
+                case 4:
+                    payBill = 40;
+                    Console.WriteLine("You have a data plan bill to pay.");
+                    Console.WriteLine("It's $40 + tax.");
+                    break;
+                case 5:
+                    payBill = 50;
+                    Console.WriteLine("You have a blorp blorp blorp bill to pay.");
+                    Console.WriteLine("It's $50 + tax.");
+                    break;
+            }
+            if (outBill == 0)
+            {
+                Console.WriteLine();
+                Console.Write(enter);
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                total = payBill * tax;
+                if ((balance < payBill) || (balance < total))
+                {
+                    Console.WriteLine("You are unable to pay this bill.");
+                    Console.WriteLine("Please come back when you have more money.");
+                }
+                else
+                {
+                    billsAmt += total;
+                    Console.WriteLine();
+                    Thread.Sleep(500);
+                    Console.WriteLine("Please wait as your bill is paid for.");
+                    sleep = 1000;
+                    Console.Write("Calculatin");
+                    TypeText("g...");
+                    Console.WriteLine();
+                    Console.WriteLine($"BILL: {payBill.ToString("C")}");
+                    Console.WriteLine($"TAX: {tax.ToString("C")}");
+                    Console.WriteLine($"TOTAL: {total.ToString("C")}.");
+                    Console.WriteLine();
+                    balance = balance - total;
+
+                    Thread.Sleep(500);
+                    Console.WriteLine("YOUR BILL HAS BEEN PAID FOR.");
+                    Console.WriteLine("Check 'Account Balance Update' for your new balance.");
+                    Console.Write(enter);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                    
+            }
+            
+
+        }
+
+        public static void AccountBalance()
+        {
+            transCount++;
+            balance = balance - transactionFee;
+            Console.WriteLine("--------ACCOUNT BALANCE UPDATE--------");
+            Console.WriteLine($"Your current balance is {balance.ToString("C")}.");
+            Console.WriteLine($"You've been charged a total of {(transCount * transactionFee).ToString("C")} in transactions.");
+            Console.WriteLine($"You deposited a total of {depositAmt.ToString("C")}.");
+            Console.WriteLine($"You withdrew a total of {withdrawAmt.ToString("C")}.");
+            Console.WriteLine($"You paid bills worth a total of {billsAmt.ToString("C")}. [includes tax]");
+            Console.WriteLine();
+            Thread.Sleep(100);
+            Console.Write(enter);
+            Console.ReadLine();
+            Console.Clear();
         }
 
         public static void Doubles()
